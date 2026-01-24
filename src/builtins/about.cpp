ef6c78d2bd414c6e98480dc8d162a1b42f265d1e
simon0302010 <simon@hackclub.app>
2026-01-24 12:02:29 +0100

    Update about menu

--- src/builtins/about.cpp
+++ src/builtins/about.cpp
@@ -8,9 +8,9 @@ int show_about() {
     println("bsh Version 0.2");
     println("");
     println("bsh  Copyright (C) 2026  simon0302010");
-    println("This program comes with ABSOLUTELY NO WARRANTY; for details type `show w`");
+    println("This program comes with ABSOLUTELY NO WARRANTY.");
     println("This is free software, and you are welcome to redistribute it");
-    println("under certain conditions; type `show c` for details.");
+    println("under certain conditions.");
 
     return 0;
 }
